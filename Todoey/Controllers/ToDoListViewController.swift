34ed39cc32353a07478890cefddfbe97b2ae91c9
THOTA HARIPRASAD <thotahariprasad@thotas-macbook-air.local>
2018-02-26 12:02:55 +0530

    added custom data object and checkmark problem solved

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -12,43 +12,88 @@ class ToDoListViewController: UITableViewController {
     
     let defaults = UserDefaults.standard
     
-   var itemArray = ["hari","ravi","srinu"]
+   var itemArray = [Item]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "ToDoList Array") as? [String] {
-            itemArray = items
-        }
+    print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
+        
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "buy eggs"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "go home"
+        itemArray.append(newItem3)
         
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
+            itemArray = items
+        }
     }
+    
+    
+    
+    //MARK: - number of sections
+    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return itemArray.count
     }
 
+    
+    
+    
+    
+    
+    //MARK: - CELL FOR ROW AT INDEXPATH
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        
+        let item = itemArray[indexPath.row]
+        
+        
+        cell.textLabel?.text = item.title
+        
+        //terinary operator
+        
+        //value = condition ? valueiftrue : valueiffalse
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        
         return cell
         
     }
+    
+    
+    
+    //MARK: - DID SELECT ROW
+    
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
         
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK: - BARBUTTON PRESSED
     
     
     @IBAction func addItem(_ sender: UIBarButtonItem) {
@@ -59,16 +104,23 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction.init(title: "Add Item", style: .default) { (action) in
 //            what will happen when we click add item
-           self.itemArray.append(textField.text!)
-            self.defaults.set(self.itemArray, forKey: "ToDoList Array")
+            
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+           self.itemArray.append(newItem)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            
             self.tableView.reloadData()
         }
         
-        alert.addAction(action)
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "enter new"
             textField = alertTextField
         }
+        alert.addAction(action)
+        
         present(alert, animated: true, completion: nil)
     }
     
---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by THOTA HARIPRASAD on 24/02/18.
+//  Copyright © 2018 THOTA HARIPRASAD. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
